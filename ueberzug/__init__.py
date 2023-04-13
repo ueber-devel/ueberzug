03527a2ef175ba654bf82d49102a7eec96f3cdeb
eylles <ed.ylles1997@gmail.com>
2023-04-12 20:21:57 -0600

    formatting

--- ueberzug/__init__.py
+++ ueberzug/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '18.1.9'
 __license__ = 'GPLv3'
-__description__ ='ueberzug is a command line util which allows to display images in combination with X11'
+__description__ = 'ueberzug is a command line util which allows to display images in combination with X11'
 __url_repository__ = 'https://github.com/ueber-devel/ueberzug'
 __url_bug_reports__ = 'https://github.com/ueber-devel/ueberzug/issues'
 __url_project__ = __url_repository__
